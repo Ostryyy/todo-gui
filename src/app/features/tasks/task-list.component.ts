a8b6f7c96a15d4c3ac7205705dff44cefd105e27
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-10-21 22:50:00 +0200

    feat: implement task list component with dialog and CRUD actions

--- src/app/features/tasks/task-list.component.ts
+++ src/app/features/tasks/task-list.component.ts
@@ -1,15 +1,59 @@
-import { Component, DestroyRef, inject } from '@angular/core';
-import { AuthService } from '../../core/auth/auth.service';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskStore } from '../../core/tasks/tasks.store';
+import { Task } from '../../core/models/task.model';
+import { ConfirmDialogComponent } from './items/confirm-dialog/confirm-dialog.component';
+import { TaskDialogComponent } from './items/task-dialog/task-dialog.component';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-task-list',
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+  ],
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.scss'],
 })
-export class HomeComponent {
+export class TaskListComponent implements OnInit {
+  protected taskStore = inject(TaskStore);
+  public dialog = inject(MatDialog);
 
- 
+  ngOnInit() {
+    this.taskStore.loadTasks();
+  }
+
+  openTaskDialog(task?: Task) {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      data: task || { title: '', description: '', status: 'todo' },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        if (task) {
+          this.taskStore.updateTask(task._id!, result);
+        } else {
+          this.taskStore.addTask(result);
+        }
+      }
+    });
+  }
+
+  confirmDelete(task: Task) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: { title: task.title },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.taskStore.removeTask(task._id!);
+      }
+    });
+  }
 }
