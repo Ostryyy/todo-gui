b6dd6f8444174d282069b26c955ab1f83ebd1b8a
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-10-29 20:46:26 +0100

    Changed app name and improved destroying of sub in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AuthStore } from './core/auth/auth.store';
 import { AppStore } from './app.store';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -24,7 +25,7 @@ import { AppStore } from './app.store';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  title = 'gui';
+  title = 'TODO';
   protected appStore = inject(AppStore);
   protected authStore = inject(AuthStore);
   private router = inject(Router);
@@ -33,7 +34,7 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.authStore.loadTokenFromStorage();
 
-    this.router.events.subscribe((event) => {
+    this.router.events.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((event) => {
       if (event instanceof NavigationStart) {
         this.appStore.setLoading(true);
       } else if (event instanceof NavigationEnd) {
